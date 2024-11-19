bcb268d349b40addbc0190407f37f1d723e7aa3e
Mostafa Rafiki <rafikimostafa@gmail.com>
2024-11-19 22:41:11 +0100

    nice

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
-    }
-  }
-})
+      "/api/": "https://shopin-backend-kngmelpnl-raf-mos-projects.vercel.app/",
+      "/uploads/":
+        "https://shopin-backend-kngmelpnl-raf-mos-projects.vercel.app/",
+    },
+  },
+});
