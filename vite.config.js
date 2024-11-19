92eb6b445eef26a7eca2cb0786ffb001540d5e82
Mostafa Rafiki <rafikimostafa@gmail.com>
2024-11-19 22:45:37 +0100

    nice

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["lucide-react"],
+    },
+  },
   server: {
     proxy: {
       "/api/": "https://shopin-backend-kngmelpnl-raf-mos-projects.vercel.app/",
